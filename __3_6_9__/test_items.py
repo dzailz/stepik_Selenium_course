5fada95b2087c330e2a9eedea4d5fc0ecf02da8b
dzailz <katin.v.v@gmail.com>
2019-07-03 10:18:55 +0300

    Added '__3_6_9__' folder for checking 3.6.9 task

---
+++ __3_6_9__/test_items.py
@@ -0,0 +1,9 @@
+#  import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_language_transmission(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector(".btn-add-to-basket")
+    #  time.sleep(10) #  Please, turn on for check "fr"
