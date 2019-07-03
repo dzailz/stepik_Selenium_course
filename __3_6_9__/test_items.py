ecd09fcb2fd27efd053d7b6f3380440797c4bbcf
dzailz <51898657+dzailz@users.noreply.github.com>
2019-07-03 10:39:55 +0300

    Update test_items.py
    add a sleep()

--- __3_6_9__/test_items.py
+++ __3_6_9__/test_items.py
@@ -1,4 +1,4 @@
-#  import time
+import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
@@ -6,4 +6,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_language_transmission(browser):
     browser.get(link)
     browser.find_element_by_css_selector(".btn-add-to-basket")
-    #  time.sleep(10) #  Please, turn on for check "fr"
+    time.sleep(10) #  Please, turn on for check "fr"
