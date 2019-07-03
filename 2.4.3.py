ffb46618f2b71a76933f0155446215024cac3ecd
dzailz <51898657+dzailz@users.noreply.github.com>
2019-07-03 10:25:02 +0300

    Delete 2.4.3.py

--- 2.4.3.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Chrome()
-browser.get("http://suninjuly.github.io/wait1.html")
-browser.implicitly_wait(5)
-browser.find_element_by_css_selector(".btn").click()
-message = browser.find_element_by_id("check_message")
-assert "успешно" in message.text
