dacd800751a32217f7ff7fcf6b9bfdc486a00a1b
dzailz <51898657+dzailz@users.noreply.github.com>
2019-07-03 15:02:33 +0300

    Update test_items.py
    assert added

--- __3_6_9__/test_items.py
+++ __3_6_9__/test_items.py
@@ -5,5 +5,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_language_transmission(browser):
     browser.get(link)
-    browser.find_element_by_css_selector(".btn-add-to-basket")
-    time.sleep(10) #  Please, turn on for check "fr"
+    add_to_basket = browser.find_element_by_css_selector(".btn-add-to-basket")
+    assert add_to_basket
+    add_to_basket.click()
+    time.sleep(10) #  Please turn on for check "fr"
+    
